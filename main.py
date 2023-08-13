a4f13b294ecbe00c3870319563d90718f1b26b5e
bo1690 <85517465+bo1690@users.noreply.github.com>
2023-08-13 13:44:09 +0000

    Added template inheritance - content shared with other templates

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import datetime
 from flask import Flask, render_template
+
 
 main = Flask(__name__)
 
 @main.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', curryear = datetime.datetime.today().year)
 
 if __name__ == "__main__":
     main.run(host="0.0.0.0", debug=True)
