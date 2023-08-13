69154677251bfbea3d044297118cfaa35d09cc22
bo1690 <85517465+bo1690@users.noreply.github.com>
2023-08-13 19:11:44 +0000

    Added api route and requirements.txt

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 PLAYERS = [
   {
     'id:':1,
     'name': 'Jack Butland',
-    'position': 'Goalkeeper'
+    'position': 'Goalkeeper',
+    'country': 'England'
   },
     {
     'id:':2,
     'name': 'Connor Goldson',
-    'position': 'Defender'
+    'position': 'Defender',
+    'country': 'England'
   },
     {
     'id:':3,
     'name': 'Todd Cantwell',
-    'position': 'Midfielder'
+    'position': 'Midfielder',
+    'country': 'England'
   },
     {
     'id:':4,
     'name': 'Danilo',
-    'position': 'Forward'
+    'position': 'Forward',
+    'country': 'Brazil'
   }
 ]
 
@@ -29,6 +33,11 @@ main = Flask(__name__)
 @main.route("/")
 def hello_world():
     return render_template('home.html', players=PLAYERS, curryear = datetime.datetime.today().year)
+
+##API - NOT DOING ANYTHING WITH IT AT THE MOMENT
+@main.route("/api/players")
+def list_players():
+  return jsonify(PLAYERS)
 
 if __name__ == "__main__":
     main.run(host="0.0.0.0", debug=True)
